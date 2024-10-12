b3130921213f41e8e2516ac1d13bd6f278569093
alsami6 <alsami1209@gmail.com>
2024-10-12 22:21:34 +0600

    _

--- Practice-Problem/even-sum.c
+++ Practice-Problem/even-sum.c
@@ -3,13 +3,17 @@
 int main (){    
     int n;
     scanf("%d", &n);
-    int sum;
-    int ld;
+    int sum = 0;
+    // int ld = 0;
 
-    while(n != 0){
+    for(int ld = 0; n != 0; n = n/10){
+
         ld = n % 10;
-        sum = sum + ld;
-        n = n / 10;
+
+        if(ld % 2 == 0){
+            sum = sum + ld;
+        }
+
     }
 
     printf("%d", sum);
